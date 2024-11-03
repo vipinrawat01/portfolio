2d03d9fff509bae58b4850cec96433c4b80e5fc1
vipinrawat01 <165695261+vipinrawat01@users.noreply.github.com>
2024-11-03 21:26:21 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,9 +21,9 @@ const Home = () => {
         <div className='mb-11 md:mb-6 md:mt-16 md:flex w-screen min-h-80 md:min-h-85 top-10 md:top-6 flex-row gap-30 relative' id='home'>  
             <Spline className='absolute'
                 scene="https://prod.spline.design/srkgRox2hmNXJLf8/scene.splinecode" />
-            <div className='absolute -mt-60 top-40 md:top-80'>
+            <div className='absolute -mt-40 md:-mt-60 top-40 md:top-80'>
                 <div>
-                    <h1 className='mt-16 mb-5 md:mt-0 px-6 md:px-48 text-2xl md:text-5xl font-bold text-white'>
+                    <h1 className='mt-16 mb-5 md:mt-0 px-4 md:px-48 text-2xl md:text-5xl font-bold text-white'>
                         {"Hi I'm "}<br></br>
                         <span className='wrap text-3xl md:text-6xl text-purple-500'>{text}</span>
                         <Cursor />
@@ -38,9 +38,10 @@ const Home = () => {
                     </p>
                     <button 
                         onClick={() => navigate('/resume')} // Use navigate instead of history.push
-                        className='mt-4 inline-block md:ml-48 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300'>
-                        View Resume
+                        className='mt-2 md:mt-4 inline-block ml-4 md:ml-48 px-3 py-1 md:px-4 md:py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-800 transition duration-300'>
+                        <p className='text-xs md:text-xl'>View Resume</p>
                     </button>
+
                 </div>
             </div>
         </div>
